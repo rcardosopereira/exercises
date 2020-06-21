15d849b41b99ac57472398f3a29ee4b0091a7e7e
Rafael Pereira <rcardosopereira@gmail.com>
2020-06-21 01:16:38 -0300

    First Commit

---
+++ src/menorValor.java
@@ -0,0 +1,58 @@
+/* 
+ * Exercício
+ * Ask the user 10 numbers and insert them into an array of integer and show the lowest value inserted by user.
+ * Fonte: https://www.guj.com.br/t/maior-e-menor-valor-resolvido/91031
+ * https://js.do/
+ * <script>
+
+document.write("<b>Qual é o menor?</b><br><br>");
+document.write("A segunte lista de valores aleatórios foi gerada automaticamente:<br>");
+
+var lista = [];
+
+for(i=0; i<10;i++){
+	lista[i] = Math.round(Math.random()*10);
+    document.write("- " + lista[i] + "<br>");
+}
+
+var menor = lista[0];
+
+for (i=0; i<lista.length; i++){
+	if(lista[i] < menor){
+    	menor = lista[i];
+    }
+}
+
+document.write("<br><b>O menor número da lista é o \"" + menor + "\" !!!</b><br><br>");
+
+</script>
+ * 
+ * 	
+ */
+
+import java.util.Scanner;
+
+public class menorValor {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		int valor[] = new int[5];
+		
+			System.out.print("Digite  "+valor.length+" valore(s): ");
+			for(int i = 0; i < valor.length; i++){
+			valor[i] = sc.nextInt();
+			}
+			int min=valor[0];
+			for(int i = 0; i < valor.length; i++){
+				if(min > valor[i] ) {
+					min = valor[i]; 
+				}
+			}
+			System.out.println("The minimum value is: "+ min);
+		}
+	}
+
+
+
+
+
---
+++ src/minimumValue.java
@@ -0,0 +1,36 @@
+/*
+ * Rafael C. Pereira
+ * Data: 20/Jun/2020
+ * Exercise:
+ * Ask the user 10 numbers and insert them into an array of integer and show the lowest value inserted by user.
+ * 
+ */
+
+
+import java.util.Scanner;
+
+public class minimumValue {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		int value[] = new int[5];
+
+		for(int i = 0; i < value.length; i++){
+			System.out.print("Enter the " +(i+1)+"º value: ");
+			value[i] = sc.nextInt();
+		} 
+
+		int min=value[0];
+		for(int i = 0; i < value.length; i++){
+			if(min > value[i] ) {
+				min = value[i];
+			}
+		}
+		System.out.println("The minimum value is: "+ min);
+	}
+}
+
+
+
+
